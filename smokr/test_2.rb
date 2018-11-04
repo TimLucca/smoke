2445e69f31653ecd6e85dc7f4da0cad06c4c9636
MarlonLop <2923927+MarlonLop@users.noreply.github.com>
2018-11-04 13:15:05 -0700

    minitest working with require_relative test2

---
+++ smokr/test_2.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+
+# test class
+class SimpleClass2
+  def out_2
+    'Hello!'
+  end
+end
+
+# # comment
+# class SimpleTest < Minitest::Test
+#   def setup
+#     @y = SimpleClass.new
+#   end
+#
+#   def test_hello
+#     assert_equal 'Hello!', @y.out
+#   end
+# end
