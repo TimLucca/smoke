371ae1ac211ded87959cacaca18b94c54feeae66
Michael Newman <newms226@gmail.com>
2018-11-06 21:24:37 -0700

    fixed imports

---
+++ smokr/test.rb
@@ -0,0 +1,1 @@
+p [*$<][1].split.min_by{|l|v=l.to_i;v**4-v}.to_i
--- smokr/test/helper.rb
+++ smokr/test/helper.rb
@@ -5,8 +5,3 @@ require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 require_relative 'test_runner'
-
-require 'test/unit'
-
-class Test::Unit::TestCase
-end
